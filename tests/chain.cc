2ff6b40b2e1834827de9ff1365ff8c7092309142
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2023-10-28 16:29:19 +0200

    shared lib

---
+++ tests/chain.cc
@@ -0,0 +1,25 @@
+#include<vector>
+#include<iostream>
+
+#ifdef ALIB
+void b(std::vector<int*>&v);
+void a(std::vector<int*>&v) {
+   std::cout << "in alib" << std::endl;
+   v.push_back(new int(3));
+   b(v);
+}
+#endif
+
+#ifdef BLIB
+#include <iostream>
+#include <stacktrace>
+void b(std::vector<int*>&v) { 
+   std::cout << "in blib" << std::endl;
+   v.push_back(new int(4));
+   std::cout << std::stacktrace::current() << std::endl;
+}
+#endif
+
+
+
+
