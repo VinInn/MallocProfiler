5cd2b3f0ec9c032d4500b7fdbd39c961e00302c2
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2023-11-09 12:40:17 +0100

    works

--- include/argumentRemover.h
+++ include/argumentRemover.h
@@ -3,7 +3,7 @@
 #include<string>
 
 // find the closing ">" in string s starting at position pos
-size_t argumentRemover(std::string const & s, size_t pos) {
+size_t identifyArgument(std::string const & s, size_t pos) {
   // find first
   for (;pos<s.size(); ++pos) {
     if ('<' ==s[pos] ) break;
@@ -17,4 +17,21 @@ size_t argumentRemover(std::string const & s, size_t pos) {
   return std::string::npos;
 }
 
+// rmove argument that follows p
+inline
+bool argumentRemover(std::string & s, size_t pos) {
+  auto p = identifyArgument(s,pos);
+  if (p==std::string::npos) return false;
+  s.erase(pos, p-pos+1);
+  return true;
+}
 
+// remove all occurence of ", Sym<...>" in string s
+inline 
+void removeTemplate(std::string & s, std::string const & Sym)  {
+  std::string target = ", " + Sym;
+  size_t p;  
+  while (std::string::npos != (p=s.find(target))) {
+    if (!argumentRemover(s,p)) return;
+  }
+}
--- tests/argumentRemover.cpp
+++ tests/argumentRemover.cpp
@@ -17,20 +17,34 @@ using WWI = W<W<W<W<int>>>>;
 using WWF = W<W<W<W<float>>>>; 
 using P = std::pair<WWI,WWF>;
 using V = std::vector<P>;
+using VV = std::vector<V>;
+using VVV = std::vector<std::pair<std::string,VV>>;
 
+void testRemover(std::string symNameV) {
+
+  std::cout << "Original " << symNameV << std::endl;
+
+  removeTemplate(symNameV,"std::allocator");
+  std::cout << "Cleaned " << symNameV << std::endl;
+  if (std::string::npos!=symNameV.find("std::allocator")) std::cout << "FAILED" << std::endl;
+}
 
 int main() {
 
   std::cout << typeid(V).name() << std::endl;
 
-  std::string symName = "std::vector<std::pair<W<W<W<W<int> > > >, W<W<W<W<float> > > > >, std::allocator<std::pair<W<W<W<W<int> > > >, W<W<W<W<float> > > > > > >";
+  std::string symNameV = "std::vector<std::pair<W<W<W<W<int> > > >, W<W<W<W<float> > > > >, std::allocator<std::pair<W<W<W<W<int> > > >, W<W<W<W<float> > > > > > >";
 
-  std::cout << symName << std::endl;
 
-  auto pos = symName.find(", std::allocator");
-  auto p = argumentRemover(symName,pos);
-  if (p!=std::string::npos)  symName.erase(pos, p-pos+1);
-  std::cout << symName << std::endl;
+  testRemover(symNameV);
+
+  std::cout << typeid(VVV).name() << std::endl;
+
+  std::string symNameVVV = "std::vector<std::pair<std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> >, std::vector<std::vector<std::pair<W<W<W<W<int> > > >, W<W<W<W<float> > > > >, std::allocator<std::pair<W<W<W<W<int> > > >, W<W<W<W<float> > > > > > >, std::allocator<std::vector<std::pair<W<W<W<W<int> > > >, W<W<W<W<float> > > > >, std::allocator<std::pair<W<W<W<W<int> > > >, W<W<W<W<float> > > > > > > > > >, std::allocator<std::pair<std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> >, std::vector<std::vector<std::pair<W<W<W<W<int> > > >, W<W<W<W<float> > > > >, std::allocator<std::pair<W<W<W<W<int> > > >, W<W<W<W<float> > > > > > >, std::allocator<std::vector<std::pair<W<W<W<W<int> > > >, W<W<W<W<float> > > > >, std::allocator<std::pair<W<W<W<W<int> > > >, W<W<W<W<float> > > > > > > > > > > >";
+
+
+  testRemover(symNameVVV);
+
 
   return 0;
 }
