ed717f71891f03ddd39bb59c2b0f29e23811942f
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2023-11-26 14:24:53 +0100

    document API

--- include/mallocProfiler.h
+++ include/mallocProfiler.h
@@ -39,37 +39,55 @@ namespace mallocProfiler {
    constexpr bool allThreads=true;
    constexpr bool currentThread=false;
 
+   // return true if the profiler has been correcly preloaded
    bool loaded();
 
+   // return true if the  profiler is active for the current thread or for all threads (depending on the argument)
    bool active(bool allThreads=true);
+   // return true if the profiler is globally enabled
    bool enabled ();
 
+   // enable the profiler (all instrumentation)
    void enable ();
+   // disable the profiler (all instrumentation)
    void disable ();
+   // activate the  profiler for the current thread or for all threads (depending on the argument)
    void activate(bool allThreads=true);
+   // activate the  profiler for the current thread or for all threads (depending on the argument)
    void deactivate(bool allThreads=true);
+   // clear all data for the current thread or for all threads (depending on the argument)
    void clear(bool allThreads=true);
 
+   // set the threshold above which stacktraces are recorded (soes not affect global statistics)
    void setThreshold(std::size_t value, bool reverse=false);
+   // return the current threshold
    std::size_t getThreshold();
 
+   // print some "debug" info
    void setVerbose(bool isVerbose);
-
+   // return the statistics  for the current thread or for all threads (depending on the argument)
    Stat summary(bool allThreads=true);
 
+   // disable the dump at the end of the process 
    void noFinalDump();
 
+   // do not report stacktraces for which memlive is zero
    void noZeroLiveDump(bool);
 
+   // dump al lstacktraces separating the stat fileds with a custom character, sorting them (not implemented)
    std::ostream &  dump(std::ostream &, char separator='$', SortBy mode=SortBy::none, bool allThreads=true);
    
+   // dump stacktraces with one symbol per line including source file name and line number, stop at current function or to the first occurent of string "from" in the sybol name
    std::ostream &  dumpDetails(std::ostream &, std::string const & from, bool allThreads=true);
 
+   // set the dumping intervals in case the dumping threads are loaded
    void setDumpingInterval(std::chrono::seconds const & summaryLap, std::chrono::seconds const & dumpLap);
 
-
+  // set a custom remangler callback
   void setRemangler(remangleType);
+  // return the default remangler callback
   remangleType getDefaultRemangler();
+  // return the current remangler callback
   remangleType getCurrentRemangler();
 
 }
